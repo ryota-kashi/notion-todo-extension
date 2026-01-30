a2f58dc8e6b5d4801cd931d22647461f5f809619
shgm8 <94514740+shgm8@users.noreply.github.com>
2026-01-31 02:08:29 +0900

    バグfix

--- src/sidepanel.js
+++ src/sidepanel.js
@@ -345,7 +345,7 @@ function createTodoElement(todo) {
   
   // Googleカレンダー追加ボタン (常に表示)
   const googleBtnHtml = `
-    <button class="icon-btn google-task-btn" title="Googleカレンダーに追加">
+    <button class="task-action-btn google-task-btn" title="Googleカレンダーに追加">
       <svg viewBox="0 0 24 24" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" focusable="false">
         <path fill="currentColor" d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20a2 2 0 002 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zm0-12H5V6h14v2zm-7 5h5v5h-5v-5z"></path>
       </svg>
